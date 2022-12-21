b80d0936cd9c5d0956dfb510b72831d50b04b1ca
jeeves1618 <jeeves1618@gmail.com>
2022-12-21 07:21:43 +0530

    My SQL from NodeJS

---
+++ 05 Blogger/data/authors.sql
@@ -0,0 +1,26 @@
+INSERT INTO `blogger`.`authors`
+(
+`name`,
+`email`)
+VALUES
+(
+'Richard Dawkins',
+'rdawkins@test.com');
+
+INSERT INTO `blogger`.`authors`
+(
+`name`,
+`email`)
+VALUES
+(
+'Ayn Rand',
+'iamrand@test.com');
+
+INSERT INTO `blogger`.`authors`
+(
+`name`,
+`email`)
+VALUES
+(
+'Christopher Hitchens',
+'hitched@test.com');
---
+++ 05 Blogger/data/create_author_script.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `authors` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
